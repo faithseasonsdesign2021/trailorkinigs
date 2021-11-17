a6f0da4c5084dcf522cd34c79e294fc992bb2261
faithseasonsdesign2021 <91766642+faithseasonsdesign2021@users.noreply.github.com>
2021-11-17 13:15:44 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -3,4 +3,19 @@ function navigationButton(){
         $(".sm-navigation-list-wrapper").toggle();
     });
 }
+
+
+function navigationList(){
+    let window_width = window.outerWidth;
+    let window_height = window.outerHeight;
+
+    if(window_height <= 568){
+        let list = document.getElementsByClassName("sm-navigation-list");
+        list[0].style.top = "85vh";
+       
+        //welcome_caption[0].style.fontSize = "14pt";
+    }
+
+}
+navigationList();
 navigationButton();
